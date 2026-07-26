c1e82f4c1fbcb0dcb506276a4abf836928967d49
David Adeleke <adeleke.david2017@gmail.com>
2026-07-26 23:56:29 +0100

    ingress with SG-3

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -15,6 +15,8 @@ data "aws_ami" "amazon_linux" {
 
 resource "aws_security_group" "ec2" {
   vpc_id = var.vpc_id
+
+}
   
   # 2. Add your persistent, tracked ports as standalone resources
 
@@ -33,7 +35,7 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
   ip_protocol       = "-1" # Semantically matches your original protocol = "-1"
 }
 
-}
+
 
 resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon_linux.id
