46de97e481c0d9d35ac2cb55eafb01340707068c
David Adeleke <adeleke.david2017@gmail.com>
2026-07-26 23:33:34 +0100

    ingress 80 and 443

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -16,7 +16,21 @@ data "aws_ami" "amazon_linux" {
 resource "aws_security_group" "ec2" {
   vpc_id = var.vpc_id
   
-  ingress = [] 
+  # Allow HTTP (Port 80) from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow HTTPS (Port 443) from anywhere
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress { 
     from_port = 0 
